dd77f142325dc7a1d1e68155f957b31dced3a118
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-10-24 16:57:20 -0500

    images are flipping with descriptions on click

--- src/components/GalleryItems/GalleryItems.js
+++ src/components/GalleryItems/GalleryItems.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 
 class GalleryItems extends Component {
 
+    // state to determine whether to show image or description
+    state = {
+        showImage: true
+    }
 
+    // function to switch showImage state upon click
+    showImageUpdate = () => {
+        console.log('clicked', this.state.showImage);
+
+        this.setState( {
+            showImage: !this.state.showImage
+        })
+    }
 
     render() {
         return(
-            <img src={this.props.image.path} alt={this.props.image.description} width="500" height="600"/>
-        )
+            <div>
+                {this.state.showImage ? 
+                <img src={this.props.image.path} 
+                alt={this.props.image.description} 
+                width="500" height="600"
+                onClick={this.showImageUpdate}/>
+                :
+                <p onClick={this.showImageUpdate}>{this.props.image.description}</p>
+                }
+            </div>   
+        )  
     }
 }
 
